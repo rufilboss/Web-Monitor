0ef9297b06b50dc2cfba02486c0f228f0292251f
Yusadolat <Yusadolat@gmail.com>
2020-04-16 05:56:08 +0100

    🏷️ Refoctor Code to look nicer :zap:

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.get("/", (req, res) => {
+const getRoot = (req, res) => {
   res.json({ status: "success" });
-});
+};
 
-// variable should be outside of the API's scope.
-let data = null;
+let data = {
+  message: "Success",
+  data: {
+    name: "Yusuf",
+    Ocuppation: "Software Engineer",
+  },
+};
 
-app.post("/data", (req, res) => {
+const createData = (req, res) => {
   // We assign the body of the request to the data variable.
   data = req.body;
   // We send the data variable in the response.
   res.json(data);
-});
+};
 
-app.get("/data", (req, res) => {
+const getData = (req, res) => {
   //data comes from the variable we declared and mutated earlier
   res.json(data);
-});
+};
+
+app.get("/", getRoot);
+
+// variable should be outside of the API's scope.
+
+//app.post("/data", createData);
+
+//app.get("/data", getData);
+
+app.route("/data").get(getData).post(createData);
 
 app.listen(3000, () => console.log("Listening on port 3000"));
