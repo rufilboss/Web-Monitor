d6999cbadf9275f1142641d73a079ebaefc617dc
Yusadolat <Yusadolat@gmail.com>
2020-05-07 15:46:02 +0100

    📦 Secure DB URL and Setting ENV

--- db/index.js
+++ db/index.js
@@ -1,18 +1,18 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 // We create a url variable
 let dbUrl = null;
 
 // If NODE_ENV equals 'test' we will use the cloud test database
-if(process.env.NODE_ENV === 'test') dbUrl = 'postgres://utvdnzdb:hxLot3tUXkVkmG66z7uQlO05f-N1rjun@packy.db.elephantsql.com:5432/utvdnzdb';
+if (process.env.NODE_ENV === "test") dbUrl = process.env.dbUrl;
 else {
-	// If not we will use our production database
-	dbUrl = 'postgres://ivan:123456@localhost:5432/users';
+  // If not we will use our production database
+  dbUrl = "postgres://yusuf:123456@localhost:5432/yusuf";
 }
 // We initialize with our database url variable
 const sequelize = new Sequelize(dbUrl, {
-  host: 'localhost',
-  dialect: 'postgres',
+  host: "localhost",
+  dialect: "postgres",
   // I shut off logging because it's annoying
   logging: false,
   operatorsAliases: false,
@@ -21,8 +21,8 @@ const sequelize = new Sequelize(dbUrl, {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 });
 
 module.exports = sequelize;
