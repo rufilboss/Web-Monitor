7a520e511be12bd3a3bee3e0fb295ad53e68edf0
Hammed Oyedele <itz.harmid@gmail.com>
2020-10-30 09:17:16 +0100

    refactor(db): change if statements to ternary operation

--- db/index.js
+++ db/index.js
@@ -1,14 +1,12 @@
 const Sequelize = require("sequelize");
 
 // We create a url variable
-let dbUrl = null;
-
 // If NODE_ENV equals 'test' we will use the cloud test database
-if (process.env.NODE_ENV === "test") dbUrl = process.env.dbUrl;
-else {
-  // If not we will use our production database
-  dbUrl = "postgres://yusuf:123456@localhost:5432/yusuf";
-}
+// If not we will use our production database
+let dbUrl = process.env.NODE_ENV === "test" 
+            ? process.env.dbUrl 
+            : "postgres://yusuf:123456@localhost:5432/yusuf";
+
 // We initialize with our database url variable
 const sequelize = new Sequelize(dbUrl, {
   host: "localhost",
